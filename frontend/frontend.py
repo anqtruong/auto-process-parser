a43404b5d545ce8fd50818d02a69dfe5f4878d89
An Truong <anqtruong77@gmail.com>
2026-06-29 11:34:21 -0500

    Implemented simple frontend (no backend yet).

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,10 +1,6 @@
 import requests
 import streamlit as st
 
-response = requests.get("http://127.0.0.1:8000")
-st.write(response)
-
-"""
 st.title("Auto Process Parser by An Truong")
 st.header("")
 st.header("Please submit your document (PDF).")
@@ -14,4 +10,3 @@ if uploadedFile is not None:
     st.success("File successfully uploaded!")
 else:
     st.error("File was not successfully uploaded, please try again.")
-"""
