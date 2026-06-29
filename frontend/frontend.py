51b17578f291a8e86b69bad26cc3bb47387eb453
An Truong <anqtruong77@gmail.com>
2026-06-29 11:40:11 -0500

    Properly tested MinerU api connection

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -10,3 +10,11 @@ if uploadedFile is not None:
     st.success("File successfully uploaded!")
 else:
     st.error("File was not successfully uploaded, please try again.")
+
+#Test MinerU API connection
+"""try:
+    response = requests.get("http://127.0.0.1:8000/health")
+    st.write(response.json())
+
+except requests.exceptions.ConnectionError:
+    st.error("Backend couldn't be reached")"""
